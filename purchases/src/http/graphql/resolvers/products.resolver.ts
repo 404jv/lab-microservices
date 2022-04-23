74445b1e1e913d76304c3997a3489875506a2a26
Joao Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2022-04-23 18:04:46 -0300

    pruchases

---
+++ purchases/src/http/graphql/resolvers/products.resolver.ts
@@ -0,0 +1,19 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Product } from '../models/product';
+import { ProductsService } from '../../../services/products.service';
+import { CreateProductInput } from '../inputs/create-product-input';
+
+@Resolver()
+export class ProductsResolver {
+  constructor(private productsService: ProductsService) {}
+
+  @Query(() => [Product])
+  products() {
+    return this.productsService.listAllProducts();
+  }
+
+  @Mutation(() => Product)
+  createProduct(@Args('data') data: CreateProductInput) {
+    return this.productsService.createProduct(data);
+  }
+}
