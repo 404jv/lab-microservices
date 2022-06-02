0a2bde7db7391e6784e6bf71e778df1cc6220521
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2022-06-02 11:17:01 -0300

    Create new resolver

--- purchases/src/http/graphql/resolvers/products.resolver.ts
+++ purchases/src/http/graphql/resolvers/products.resolver.ts
@@ -2,6 +2,8 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Product } from '../models/product';
 import { ProductsService } from '../../../services/products.service';
 import { CreateProductInput } from '../inputs/create-product-input';
+import { UseGuards } from '@nestjs/common';
+import { AuthorizationGuard } from 'http/auth/authorization.guard';
 
 @Resolver()
 export class ProductsResolver {
@@ -12,6 +14,7 @@ export class ProductsResolver {
     return this.productsService.listAllProducts();
   }
 
+  @UseGuards(AuthorizationGuard)
   @Mutation(() => Product)
   createProduct(@Args('data') data: CreateProductInput) {
     return this.productsService.createProduct(data);
--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -3,9 +3,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import path from 'node:path';
-import { ProductsService } from 'services/products.service';
+import { ProductsService } from '../services/products.service';
 import { DatabaseModule } from '../database/database.module';
 import { ProductsResolver } from './graphql/resolvers/products.resolver';
+import { PurchasesResolver } from './graphql/resolvers/purchases.resolver';
+import { PurchasesService } from '../services/purchases.service';
 
 @Module({
   imports: [
@@ -16,6 +18,14 @@ import { ProductsResolver } from './graphql/resolvers/products.resolver';
       autoSchemaFile: path.resolve(process.cwd(), 'src/schema.qql'),
     }),
   ],
-  providers: [ProductsResolver, ProductsService],
+  providers: [
+    // Resolvers
+    ProductsResolver,
+    PurchasesResolver,
+
+    // Services
+    ProductsService,
+    PurchasesService,
+  ],
 })
 export class HttpModule {}
